86fab47def054a22d849a47cbd7e8c8d18fa789f
sunnight9507 <35719387+sunnight9507@users.noreply.github.com>
2018-06-04 22:55:57 +0900

    자료구조 - 이진트리

--- asd.c
+++ asd.c
@@ -2,161 +2,98 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Que{
-	struct Que* p;
-	int num;
-	struct Que* n;
-}que;
+//14010379 권태양
 
-typedef struct Deque{
-	que *front;
-	que *rear;
-}deque;
+void makeExternalNode(char** tr,int k){
+	printf("Enter decision: ");
+	gets(tr[k]);
 
-void init(deque *dq){
-	dq->front = NULL;
-	dq->rear = NULL;
 }
 
-que* newnode(){
-	que *newnode;
-	newnode = (que*)malloc(sizeof(que));
-	newnode->num = 0;
-	newnode->n = NULL;
-	newnode->p = NULL;
-	return newnode;
-}
+void makeInternalNode(char** tr,int k){
+	char* a,an[10];
 
-int underflow(deque *dq){
-	if(dq->front == NULL && dq->rear == NULL){
-		printf("underflow");
-		return 1;
-	}
-	else return 0;
-}
+	printf("Enter question: ");
+	gets(tr[k]);
 
-void add_front(deque *dq,int num){
-	que *node=newnode();
-	node->num = num;
-	if(dq->front == NULL && dq->rear == NULL){
-		dq->front = node;
-		dq->rear = node;
+	printf("Question if yes to %s ? ",tr[k]);
+	gets(an);
+
+	if(!strcmp(an,"yes")){
+		makeInternalNode(tr,2*k);
 	}
 	else{
-		node->n = dq->front;
-		dq->front->p = node;
-		dq->front = node;		
+		makeExternalNode(tr,2*k);
 	}
-}
-
-void add_rear(deque *dq,int num){
-
-	que *node=newnode();
-	node->num = num;
-	if(dq->front == NULL && dq->rear==NULL){
-		dq->front = node;
-		dq->rear = node;
-	}
-	else{
-		node->p = dq->rear;
-		dq->rear->n = node;
-		dq->rear = node;
-	}
-}
-
-int delete_front(deque *dq){
 	
-	int n;
-	que *node = newnode();
-	n = dq->front->num;
-	node = dq->front;
-	if(dq->front == dq->rear){
-		dq->front = NULL;
-		dq->rear = NULL;
+	printf("Question if no to %s ? ",tr[k]);
+	gets(an);
+
+	if(!strcmp(an,"yes")){
+		makeInternalNode(tr,2*k+1);
 	}
 	else{
-		dq->front = dq->front->n;
-		dq->front->p = NULL;
+		makeExternalNode(tr,2*k+1);
 	}
-	free(node);
-	return n;
 }
 
-int delete_rear(deque *dq){
-	
-	int n;
-	que *node = newnode();
-	n = dq->front->num;
-	node = dq->rear;
-	if(dq->front == dq->rear){
-		dq->front = NULL;
-		dq->rear = NULL;
+char** buildDecisionTree(){
+	char **tr;
+	int i;
+
+	tr = (char**)malloc(sizeof(char*)*64);
+	for(i=1;i<=64;i++){
+		tr[i] = (char*)malloc(sizeof(char)*100);
+		tr[i][0] = '\0';
+	}
+
+	printf("***Let's build a dichotomous QA system\n");
+	makeInternalNode(tr,1);
+
+	return tr;
+}
+
+void process(char** tr,int k){
+	char an[10];
+
+	puts(tr[k]);
+	if(tr[2*k][0] == '\0' && tr[2*k+1][0] == '\0')
+		return;
+	gets(an);
+
+	if(!strcmp(an,"yes")){
+		if(tr[2*k][0] == '\0')
+			return;
+		process(tr,k*2);
 	}
 	else{
-		dq->rear = dq->rear->p;
-		dq->rear->n = NULL;
+		if(tr[2*k+1][0] == '\0')
+			return;
+		process(tr,k*2+1);
 	}
-	free(node);
-	return n;
 }
 
-void print(deque *dq){
-	
-	que *node = newnode();
-	node = dq->front;
-	while(1){
-		if(node == NULL)
-			break;
-		printf(" %d",node->num);
-		node = node->n;
-	}
-	printf("\n");
-	free(node);
+void runDecisionTree(char **tr){
+
+	printf("***Please answer questions\n");
+	process(tr,1);
+
 }
 
 int main(){
-	int n,i,num,del;
-	deque dq;
-	que *node;
-	char a[5],b[3];
+	char **tr;
+	int i;
 
-	scanf("%d",&n);
-	gets(b);
+	tr = buildDecisionTree();
 
-	init(&dq);
-
-	for(i=1;i<=n;i++){
-		scanf("%s",a);
-		if(a[0] == 'A' && a[1] == 'F'){
-			scanf("%d",&num);
-			add_front(&dq,num);
-		}
-		else if(a[0] == 'A' && a[1] == 'R'){
-			scanf("%d",&num);
-			add_rear(&dq,num);
-		}
-		else if(a[0] == 'D' && a[1] == 'F'){
-			if(underflow(&dq))
-				return 0;
-			else
-				del = delete_front(&dq);
-		}
-		else if(a[0] == 'D' && a[1] == 'R'){
-			if(underflow(&dq))
-				return 0;
-			else
-				del = delete_rear(&dq);
-		}
-		else if(a[0] == 'P'){
-			print(&dq);
-		}
+	for(i=1;i<=3;i++){
+		runDecisionTree(tr);
 	}
 
-	node = newnode();
-	node = dq.front;
-	while(node != NULL){
-		que* next = node->n;
-		free(node);
-		node = next;
+	for(i=1;i<=64;i++){
+		free(tr[i]);
 	}
+	free(tr);
+
+	return 0;
 }
